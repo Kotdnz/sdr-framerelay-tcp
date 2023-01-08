9864e41eee7a2cb0062d29ec92422083ef40a94e
Kostiantyn Nikonenko <kot.dnz@gmail.com>
2023-01-08 10:42:07 +0200

    bugfixing - wrong buff defiition in cmd thread

--- sdr-framerelay-tcp.go
+++ sdr-framerelay-tcp.go
@@ -61,7 +61,7 @@ func main() {
 func handle_cmd_stream(conSrc net.Conn, conDst net.Conn) {
 	// Handling cmd channel - from src/listening -> dst/connect
 	// buffer for source (UX)
-	var srcBuf []byte
+	srcBuf := make([]byte, 1)
 
 	for {
 		// Read data from src
